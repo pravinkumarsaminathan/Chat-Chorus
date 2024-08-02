6fbdd5ea36101758881ca072bb6db37b135c1f53
Pravinkumar <pravinkumarsaminathan@gmail.com>
2024-08-02 13:54:54 +0530

    avatar updated

--- _templates/_avatar.php
+++ _templates/_avatar.php
@@ -3,6 +3,12 @@
             <div class="avatars">
                 <img src="/Chat-Chorus/assests/male1.svg" alt="Avatar 1" class="avatar">
                 <img src="/Chat-Chorus/assests/female1.svg" alt="Avatar 2" class="avatar">
+                <img src="/Chat-Chorus/assests/grandfather.svg" alt="Avatar 5" class="avatar">
+                <img src="/Chat-Chorus/assests/grandmother.svg" alt="Avatar 6" class="avatar">
+                <img src="/Chat-Chorus/assests/malechild.svg" alt="Avatar 7" class="avatar">
+                <img src="/Chat-Chorus/assests/femalechild.svg" alt="Avatar 8" class="avatar">
+                <img src="/Chat-Chorus/assests/ninjawhite.svg" alt="Avatar 9" class="avatar">
+                <img src="/Chat-Chorus/assests/ninjablack.svg" alt="Avatar 10" class="avatar">
                 <img src="/Chat-Chorus/assests/male2.svg" alt="Avatar 3" class="avatar">
                 <img src="/Chat-Chorus/assests/female2.svg" alt="Avatar 4" class="avatar">
                 <!-- Add more avatars as needed -->
