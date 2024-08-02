2403c49d6eedb6defa6fbf391dab37bec8598111
Pravinkumar <pravinkumarsaminathan@gmail.com>
2024-08-02 13:25:06 +0530

    avatar updated

--- _templates/_avatar.php
+++ _templates/_avatar.php
@@ -1,9 +1,10 @@
 <div class="avatar-selection">
             <h2>Select an Avatar</h2>
             <div class="avatars">
-                <img src="avatar1.png" alt="Avatar 1" class="avatar">
-                <img src="avatar2.png" alt="Avatar 2" class="avatar">
-                <img src="avatar3.png" alt="Avatar 3" class="avatar">
+                <img src="assests/male1.svg" alt="Avatar 1" class="avatar">
+                <img src="assests/female1.svg" alt="Avatar 2" class="avatar">
+                <img src="assests/male2.svg" alt="Avatar 3" class="avatar">
+                <img src="assests/female2.svg" alt="Avatar 4" class="avatar">
                 <!-- Add more avatars as needed -->
             </div>
         </div>
