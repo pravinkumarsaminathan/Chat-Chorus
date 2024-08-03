700b9beacee421441d15df570a6f0f200f6064cd
Pravinkumar <pravinkumarsaminathan@gmail.com>
2024-08-03 14:00:34 +0530

    updated avatar

--- _templates/_avatar.php
+++ _templates/_avatar.php
@@ -1,16 +1,16 @@
 <div class="avatar-selection">
             <h2>Select an Avatar</h2>
             <div class="avatars">
-                <img src="/Chat-Chorus/assests/male1.svg" alt="Avatar 1" class="avatar">
-                <img src="/Chat-Chorus/assests/female1.svg" alt="Avatar 2" class="avatar">
-                <img src="/Chat-Chorus/assests/grandfather.svg" alt="Avatar 5" class="avatar">
-                <img src="/Chat-Chorus/assests/grandmother.svg" alt="Avatar 6" class="avatar">
-                <img src="/Chat-Chorus/assests/malechild.svg" alt="Avatar 7" class="avatar">
-                <img src="/Chat-Chorus/assests/femalechild.svg" alt="Avatar 8" class="avatar">
-                <img src="/Chat-Chorus/assests/ninjawhite.svg" alt="Avatar 9" class="avatar">
-                <img src="/Chat-Chorus/assests/ninjablack.svg" alt="Avatar 10" class="avatar">
-                <img src="/Chat-Chorus/assests/male2.svg" alt="Avatar 3" class="avatar">
-                <img src="/Chat-Chorus/assests/female2.svg" alt="Avatar 4" class="avatar">
+                <img src="/Chat-Chorus/assests/image1.svg" alt="Avatar 1" class="avatar">
+                <img src="/Chat-Chorus/assests/image2.svg" alt="Avatar 2" class="avatar">
+                <img src="/Chat-Chorus/assests/image3.svg" alt="Avatar 5" class="avatar">
+                <img src="/Chat-Chorus/assests/image4.svg" alt="Avatar 6" class="avatar">
+                <img src="/Chat-Chorus/assests/image5.svg" alt="Avatar 7" class="avatar">
+                <img src="/Chat-Chorus/assests/image6.svg" alt="Avatar 8" class="avatar">
+                <img src="/Chat-Chorus/assests/image7.svg" alt="Avatar 9" class="avatar">
+                <img src="/Chat-Chorus/assests/image8.svg" alt="Avatar 10" class="avatar">
+                <img src="/Chat-Chorus/assests/image9.svg" alt="Avatar 3" class="avatar">
+                <img src="/Chat-Chorus/assests/image10.svg" alt="Avatar 4" class="avatar">
                 <!-- Add more avatars as needed -->
             </div>
         </div>
